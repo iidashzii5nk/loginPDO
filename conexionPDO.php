5e4fdc9128bfc2c6b3d66cf6639b4b0033acbce8
iidashzii5nk <iifranciscoii5nk@gmail.com>
2022-05-27 11:35:38 -0500

    Importante no es bd es db ya que por eso tenia errores con la seleccion de la base de datos

--- conexionPDO.php
+++ conexionPDO.php
@@ -1,22 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-<?php
-try{
-$con = new PDO ('mysql:host=localhost; bdname=registro', 'root', '');
-echo 'Conectado';
-}catch(Exception $e){
-die('Error' . $e->getMessage());
-}
-?>
+    <?php
+    try {
+        $con = new PDO('mysql:host=localhost; dbname=registro', 'root', '');
+        
+        $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo 'Conectado';
+    } catch (Exception $e) {
+        die('Error' . $e->getMessage());
+    }
+    ?>
 </body>
+
 </html>
-
-
-
